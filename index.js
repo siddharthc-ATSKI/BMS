d4cc693e2518d382c0a5d31a989acadc77517f2f
Aniket Gajanan Panchal <aniketpanchal52648@gmail.com>
2021-11-25 21:57:47 +0530

    resolve error

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ app.get('/',(req,res)=>{
     res.render('home');
 
 })
+app.get('/register',(req,res)=>{
+      res.render('register');
+})
+
+app.get('/login',(req,res)=>{
+  res.render('login');
+})
 app.listen(3000,()=>{
     console.log('Listening');
 })
---
+++ models/movie.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+// const { schema } = require('./user');
+const schema = mongoose.Schema;
+const movieSchema = new schema({
+  Title: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Year: {
+    type: Number,
+  },
+  Rated: {
+    type: String,
+  },
+  Runtime: {
+    type: String,
+  },
+  Actor:{
+      type:String
+  },
+  Plot:{
+      type:String
+  },
+  Poster:{
+      type:String
+  },
+  Ratings:[{
+      Source:{
+          type:String
+      },
+      Value:{
+         type: String
+      }
+
+  }],
+  imdbRating:{
+      type:String
+  },
+  Director:{
+      type:String
+  }
+  ,
+  Writer:{
+      type:String
+  }
+});
