a35b994a7850549da9fd932916e497d448a31ae2
Aniket Gajanan Panchal <aniketpanchal52648@gmail.com>
2021-11-25 22:33:03 +0530

    added some data

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const path = require('path');
   // Set the default templating engine to ejs
   app.set('view engine', 'ejs');
   app.set('views', path.join(__dirname, 'views'));
-
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }))
 
 app.get('/',(req,res)=>{
     res.render('home');
@@ -14,7 +15,11 @@ app.get('/',(req,res)=>{
 app.get('/register',(req,res)=>{
       res.render('register');
 })
-
+app.get('/search', (req,res)=>{
+  // const {search}=req.body;
+console.log(req.body);
+res.send(req.body);
+})
 app.get('/login',(req,res)=>{
   res.render('login');
 })
