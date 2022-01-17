1ce14f751a0d4a589ec8b2bb52b6461613e523af
Anurag bulhe <anuragbulhe@gmail.com>
2022-01-17 23:34:11 +0530

    removed folder

--- MongoDB/server.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require('mongoose');
-
-const URL='mongodb+srv://aniket:aniket@cluster0.9fnws.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-const mongoDB= async()=>{
-    await mongoose.connect(URL);
-    console.log('Server Connected');
-}
-module.exports=mongoDB;
